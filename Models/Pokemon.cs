8769c999378c34dffce180e1279281a49bc4e717
Roman Vavrynkevych <roman.vavrynkevych@gmail.com>
2023-09-26 20:11:59 +0300

    init

---
+++ Models/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public DateTime BurthDate { get; set; }
+        public ICollection<Review> Reviews { get; set;}
+        public ICollection<PokemonOwner> PokemonOwner { get; set; }
+        public ICollection<PokemonCategory> PokemonCategory { get; set; }
+    }
+}
---
+++ Models/PokemonCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class PokemonCategory
+    {
+        public int PokemonId { get; set; }
+        public int CategotyId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Category Category { get; set; }
+    }
+}
